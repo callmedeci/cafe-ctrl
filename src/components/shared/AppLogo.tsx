ee423605b08cb9bc4c2d96e591e35a2fbca9b489
callmedeci <yunesmaghsoudie@gmail.com>
2025-10-09 20:26:54 +0330

    update the Applogo component and use the real application icon

--- src/components/shared/AppLogo.tsx
+++ src/components/shared/AppLogo.tsx
@@ -1,17 +1,26 @@
+import Logo from '@/app/icon1.png';
 import { Link } from '@/i18n/navigation';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import { H4 } from '../typography/H4';
 import { Muted } from '../typography/Muted';
-import { Drumstick as DrumstickIcon } from 'lucide-react';
-import { useTranslations } from 'next-intl';
+import { Avatar } from '../ui/avatar';
 
 function AppLogo() {
   const t = useTranslations('components');
 
   return (
     <Link className='flex items-center gap-2' href={'/dashboard'}>
-      <div className='relative'>
-        <DrumstickIcon className='text-primary' />
-      </div>
+      <Avatar className='relative size-12'>
+        <Image
+          fill
+          src={Logo}
+          alt='ctrl cafe logo'
+          placeholder='blur'
+          priority={false}
+          loading='lazy'
+        />
+      </Avatar>
 
       <div className='flex flex-col'>
         <H4>{t('appLogo.title')}</H4>
